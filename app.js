02781b6eb71d30df4d2b9b8913009f1b39e3ae02
Nicholas Defex <ndefected15@gmail.com>
2021-12-14 18:13:57 -0500

    Working on 9.3.5 Create the .gitignore

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-const fs = require("fs");
+const inquirer = require("inquirer");
 
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
+//const fs = require("fs");
+//const generatePage = require("./src/page-template");
 
-const [name, github] = profileDataArgs;
+//const pageHTML = generatePage(name, github);
 
-fs.writeFile("./index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
+//fs.writeFile("./index.html", pageHTML, (err) => {
+//  if (err) throw err;
 
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
-
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-//const printProfileData = (profileDataArr) => {
-// for (let i = 0; i < profileDataArr.length; i++) {
-//   console.log(profileDataArr[i]);
-// }
-// console.log("================");
-
-// Is the same as this...args
-//profileDataArr.forEach((profileItem) => console.log(profileItem));
-//};
-
-// printProfileData(profileDataArgs);
+//  console.log("Portfolio complete! Check out index.html to see the output!");
+//});
